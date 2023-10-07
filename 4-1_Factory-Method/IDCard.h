832433d2014eb1e13ed319ade78799be16a2370a
nishihi6 <51042248+nishihi6@users.noreply.github.com>
2023-10-08 03:43:19 +0900

    Add 4-1_Factory-Method

---
+++ 4-1_Factory-Method/IDCard.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+#include "Product.h"
+#include <string>
+#include <iostream>
+
+// メソッドuseを実装しているクラス
+class IDCard : public Product {
+public:
+    IDCard(const std::string& owner) : owner(owner) {
+        std::cout << owner << "のカードを作ります。" << std::endl;
+    }
+    void use() override {
+        std::cout << owner << "のカードを使います。" << std::endl;
+    }
+    std::string getOwner() {
+        return owner;
+    }
+private:
+    std::string owner;
+};
---
+++ 4-1_Factory-Method/main.cpp
@@ -0,0 +1,32 @@
+﻿#include "Factory.h"
+#include "IDCardFactory.h"
+#include <iostream>
+#include <string>
+#include <vector>
+#include <memory>
+
+using namespace std;
+
+// 動作テスト用のクラス
+int main() {
+    std::unique_ptr<Factory> factory = std::make_unique<IDCardFactory>();
+
+    std::unique_ptr<Product> card1(factory->create("弐四火呂"));
+    std::unique_ptr<Product> card2(factory->create("三四火呂"));
+    std::unique_ptr<Product> card3(factory->create("四四火呂"));
+
+    card1->use();
+    card2->use();
+    card3->use();
+
+    return 0;
+}
+
+//スーパークラス側で処理の骨組みを作り、サブクラス側で具体的な処理の肉付けを行うTemplate Methodパターンを、
+//インスタンス生成の場面に適用したのがFactory Methodである。
+
+//インスタンスの作り方をスーパークラス側で定めるが、具体的なクラス名までは定めない。
+//具体的な肉付けはすべてサブクラス側で行う。
+//これにより、インスタンス生成のための枠組み（フレームワーク）と、実際のインスタンス生成のクラスとを分けて考えることができる。
+
+//１つのクラスだけを見るのではなく、クラスやインタフェースの相互関係に目を向けるようにするべきである。
